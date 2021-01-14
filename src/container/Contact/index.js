8baa6d5b70714ffadcde6ca5ab6e93a48d194bc2
Tran Tu <>
2021-01-14 10:46:16 +0700

    update

--- src/container/Contact/index.js
+++ src/container/Contact/index.js
@@ -8,6 +8,8 @@ import 'swiper/swiper.scss';
 import { review } from './data';
 import SwiperCore, { Pagination } from 'swiper';
 import { DownOutlined } from '@ant-design/icons';
+import GoogleMapReact from 'google-map-react';
+import mappin from './../../asset/image/mappin.jpg'
 
 SwiperCore.use([Pagination]);
 
@@ -25,6 +27,15 @@ const Contact = () => {
   );
   const { TextArea } = Input;
 
+
+       const AnyReactComponent =({ text }) =>{
+         return(
+            <div style={{display: 'flex',flexDirection: 'row',width: '100%'}}>
+                <img src={mappin}  alt="map pin" style={{width: '20px',height:'38px',marginRight:'10px'}}/>
+                <p style={{color: 'red',fontSize:'14px'}}>Arena Multimedia</p>
+            </div>
+         )
+       }
   return (
     <div>
       <Navbar sticky={isSticky} />
@@ -137,9 +148,38 @@ const Contact = () => {
                 md={12}
                 sm={24}
                 xs={24}
-                style={{ padding: '0 50px',display: 'flex',justifyContent:'center',alignItems:'center' }}
+                style={{ padding: '0 50px',display: 'flex',flexDirection:'column' }}
               >
-                  <img src="./image/contact/googlemap.png" alt="google map" style={{width: '500',height:'500'}} />
+                  {/* <img src="./image/contact/googlemap.png" alt="google map" style={{width: '500',height:'500'}} /> */}
+                   <div style={{position:'relative',top:'18px',left:'18px',backgroundColor:'#fff',padding:'11px',boxShadow:'0px 4px 4px rgba(0, 0, 0, 0.25)',width:'191px',height:'98px',zIndex:'1000'}}>
+                    <div style={{display:'flex',flexDirection:'column'}}>
+                        <span style={{fontWeight:'400',fontSize:'10px',marginBottom:'5px'}}>Arena Multimedia</span>
+                        <span style={{color:'#565556',fontSize:'9px',letterSpacing:'0.05em',marginBottom:'5px'}}>Tầng 8, D29 Phạm Văn Bạch, Yên Hoà, Cầu Giấy , Hà Nội</span>
+                        <Rate disabled defaultValue={4} style={{color: 'grey',fontSize:'9px',marginBottom:'3px'}} />
+                        <span style={{color: '#565556',fontSize:'9px',fontWeight:'300'}}>View larger map</span>
+                    </div>
+                  </div>
+                  <div style={{width: '500x',height:'500px'}}>
+                 
+                  <GoogleMapReact
+                    bootstrapURLKeys={{ key: "AIzaSyByaVxdRZkJ4gi3J-4xy2yFSym7iLaP_KQ" }}
+                    defaultCenter={{
+                      lat: 21.025852047519663,
+                      lng: 105.78798349003321
+                    }}
+                    defaultZoom={18}
+                    style={{width:'100%',height:'100%'}}
+                    >
+                     <AnyReactComponent
+                        lat={21.025852047519663}
+                        lng={105.78798349003321}
+                        text="My Marker"
+                    />
+                    
+             </GoogleMapReact>
+                  
+
+                  </div>
               </Col>
               <Col
                 xl={12}
